e97cb6f584da2e1cf850fb0da490952dc1d37e76
David Madner <david.madner@gmail.com>
2022-09-14 09:35:27 +0200

    feat: implement search functionality

--- pages/AnimatedView.tsx
+++ pages/AnimatedView.tsx
@@ -39,6 +39,10 @@ const AnimatedView = ({
         <motion.div
           style={{
             position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
           }}
           custom={direction}
           key={view}
--- pages/Console.tsx
+++ pages/Console.tsx
@@ -1,17 +1,23 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Input, Button, Stack } from "@chakra-ui/react";
 import * as React from "react";
 
 import { Terminal } from "xterm";
+import { SearchAddon } from "xterm-addon-search";
+
 import "xterm/css/xterm.css";
 
 const Console = ({ output }: { output: string }) => {
   const nodeRef = React.useRef<HTMLDivElement>(null);
   const terminalRef = React.useRef<Terminal>();
+  const searchAddonRef = React.useRef<SearchAddon>();
 
   React.useEffect(() => {
     const initTerminal = async () => {
+      // We need to import dynamically as even deactivating ssr
+      // make NextJS fail when trying to build the app :sadpepe:
       const { Terminal } = await import("xterm");
       const { FitAddon } = await import("xterm-addon-fit");
+      const { SearchAddon } = await import("xterm-addon-search");
       const term = new Terminal();
       // Add logic with `term`
 
@@ -21,8 +27,10 @@ const Console = ({ output }: { output: string }) => {
         });
 
         const fitAddon = new FitAddon();
+        const searchAddon = new SearchAddon();
 
         terminal.loadAddon(fitAddon);
+        terminal.loadAddon(searchAddon);
 
         if (nodeRef.current) {
           terminal.open(nodeRef.current);
@@ -30,6 +38,7 @@ const Console = ({ output }: { output: string }) => {
 
         fitAddon.fit();
         terminalRef.current = terminal;
+        searchAddonRef.current = searchAddon;
       }
 
       if (output) {
@@ -49,13 +58,49 @@ const Console = ({ output }: { output: string }) => {
   }, [output]);
 
   return (
-    <Box
-      as="div"
-      height="100%"
-      overflowX="auto"
-      overflowY="auto"
-      ref={nodeRef}
-    />
+    <Box height="100%" position="relative">
+      <Box
+        as="div"
+        height="100%"
+        overflowX="auto"
+        overflowY="auto"
+        ref={nodeRef}
+      />
+      <Box
+        position="absolute"
+        right="0"
+        top="0"
+        zIndex="4"
+        margin="1"
+        backgroundColor="rgba(240,240,240,0.4)"
+        padding="2"
+        backdropFilter="blur(8px)"
+      >
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+
+            const target = event.target as typeof event.target & {
+              searchTerm: { value: string };
+            };
+
+            const searchTerm = target.searchTerm.value;
+
+            searchAddonRef.current?.findNext(searchTerm);
+          }}
+        >
+          <Stack direction="row">
+            <Input
+              _placeholder={{ color: "rgb(203,203,203)" }}
+              color="white"
+              name="searchTerm"
+              placeholder="Search logs"
+            />
+            <Button type="submit">Search</Button>
+          </Stack>
+        </form>
+      </Box>
+    </Box>
   );
 };
 
